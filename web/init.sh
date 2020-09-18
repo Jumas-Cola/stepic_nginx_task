c1d934c278da76a18250c170ed3c1d073e4c4e8e
Jumas-Cola <kbbyfl91@gmail.com>
2020-09-19 01:53:31 +0300

    add ginicorn config

--- web/init.sh
+++ web/init.sh
@@ -1,2 +1,7 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+mkdir /etc/gunicorn.d
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+cd /home/box/web
+gunicorn -c /etc/gunicorn.d/test hello:app
