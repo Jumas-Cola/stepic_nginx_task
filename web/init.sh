c46d402cdbf8128641ece1fd49c84495c68b3449
Jumas-Cola <kbbyfl91@gmail.com>
2020-09-19 21:13:23 +0300

    add django project

--- web/init.sh
+++ web/init.sh
@@ -1,8 +1,8 @@
-rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 mkdir /etc/gunicorn.d
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
-cd /home/box/web
-gunicorn -c /etc/gunicorn.d/test hello:app
+cd /home/box/web/ask
+#gunicorn -c /etc/gunicorn.d/test ask.wsgi
+gunicorn ask.wsgi
