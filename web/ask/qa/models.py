127b41dd905e0f11594c717cdf81db43e2a569f1
Jumas-Cola <kbbyfl91@gmail.com>
2020-09-20 21:49:33 +0300

    upd models

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -2,14 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class QuestionManager(models.Manager):
+
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
 class Question(models.Model):
     objects = QuestionManager()
-    title = models.CharField()
+    title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User)
-    likes = models.ManyToManyField(User)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
 
 
 class Answer(models.Model):
@@ -19,11 +28,3 @@ class Answer(models.Model):
             null=False, on_delete=models.CASCADE)
     author = models.ForeignKey(User)
 
-
-class QuestionManager(models.Manager):
-
-    def new(self):
-        return self.order_by('-added_at')
-
-    def popular(self):
-        return self.order_by('-rating')
--- web/db_comand.sh
+++ web/db_comand.sh
@@ -1,1 +1,4 @@
-mysql --host=192.168.1.201 --port=3306 -uroot -e "CREATE DATABASE db"
+sudo /etc/init.d/mysql start && mysql -uroot -e "CREATE DATABASE db"
+sudo python3 /home/box/web/ask/manage.py makemigrations
+sudo python3 /home/box/web/ask/manage.py migrate
+
