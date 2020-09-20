d8dddc2092361f3d5b828a5f4f61bc8a425cb9d6
Jumas-Cola <kbbyfl91@gmail.com>
2020-09-20 21:30:33 +0300

    add models

---
+++ web/db_comand.sh
@@ -0,0 +1,1 @@
+mysql --host=192.168.1.201 --port=3306 -uroot -e "CREATE DATABASE db"
--- web/init.sh
+++ web/init.sh
@@ -1,6 +1,7 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/mysql start
 mkdir /etc/gunicorn.d
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 cd /home/box/web/ask
